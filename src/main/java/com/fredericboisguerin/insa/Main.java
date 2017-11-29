8e468d110e5d347ba47346948ad7491cbeb471b3
tinhte96 <vanthiennhat@gmail.com>
2017-11-29 09:52:41 +0100

    commentaire "hello world" is useless

--- src/main/java/com/fredericboisguerin/insa/Main.java
+++ src/main/java/com/fredericboisguerin/insa/Main.java
@@ -2,9 +2,6 @@ package com.fredericboisguerin.insa;
 
 import com.fredericboisguerin.insa.calculateurprix.CalculateurPrixView;
 
-/**
- * Hello world!
- */
 public class Main {
     public static void main(String[] args) {
         CalculateurPrixView frame = new CalculateurPrixView();
