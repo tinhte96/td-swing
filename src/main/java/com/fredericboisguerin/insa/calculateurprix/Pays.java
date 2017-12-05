bc714dc2cd20fdca74d676cc344577e8553a31b3
tinhte96 <vanthiennhat@gmail.com>
2017-12-05 01:03:51 +0100

    country with tax rate

--- src/main/java/com/fredericboisguerin/insa/calculateurprix/Pays.java
+++ src/main/java/com/fredericboisguerin/insa/calculateurprix/Pays.java
@@ -1,7 +1,29 @@
 package com.fredericboisguerin.insa.calculateurprix;
 
 public enum Pays {
-    BEL("Belgique",0.21d),
 
+    BELGIQUE (1.21),
+    DANEMARK (1.25),
+    ALLEMAGNE (1.19),
+    GRECE (1.23),
+    ESPAGNE (1.21),
+    FRANCE (1.2),
+    IRLANDE (1.23),
+    ITALIE (1.22),
+    CHRYPE (1.19),
+    LUXEMBOURG (1.15),
+    PAYSBAS (1.21),
+    AUTRICHE (1.2),
+    PORTUGAL (1.23),
+    SUEDE (1.25);
 
+    private double tax;
+
+    Pays(double s) {
+        this.tax = s;
+    }
+
+    public double getTax(){
+        return this.tax;
+    }
 }
